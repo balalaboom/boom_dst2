8561c9997d9bc8ca0396c76ba4b0edd30b7b9489
Balalaboom <boom@hello.com>
2017-12-12 13:53:48 +0800

    123

---
+++ Caves/modoverrides.lua
@@ -0,0 +1,16 @@
+return {
+  ["workshop-1111711682"]={ configuration_options={  }, enabled=true },
+  ["workshop-362175979"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  } 
+}
